2c436d7bef5302d5580864aa95b59348c4003421
itc-n23027 <n23027@std.it-college.ac.jp>
2023-12-19 07:49:07 +0000

    4.3 hero layout

--- components/hero.js
+++ components/hero.js
@@ -2,9 +2,11 @@ import styles from 'styles/hero.module.css'
 
 const Hero = ({ title, subtitle, imageOn = false }) => (
   <div>
-    <div className={styles.text}>
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.subtitle}>{subtitle}</p>
+    <div className={styles.flexContainer}>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+      </div>
     </div>
     {imageOn && <figure> [画像] </figure>}
   </div>
