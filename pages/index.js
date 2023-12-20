a433c37d53e633d7e315607d4f5635f5fc3cd5cb
itc-n23027 <n23027@std.it-college.ac.jp>
2023-12-20 05:36:11 +0000

    4.5 Home,About,Blogのwidth調整

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 const Cube = () => {
-  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+  return (
+    <Container>
+      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+    </Container>
+  )
 }
 export default Cube
