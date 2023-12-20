256b237b7501e0c1eb9e04daf9bb608ca6f83972
itc-n23027 <n23027@std.it-college.ac.jp>
2023-12-20 06:29:15 +0000

    4.8改良

--- components/two-column.js
+++ components/two-column.js
@@ -1,13 +1,15 @@
 import styles from 'styles/two-column.module.css'
 
-export function TwoColumn ({ children }) {
+const TwoColumn = ({ children }) => {
   return <div className={styles.flexContainer}>{children}</div>
 }
 
-export function TwoColumnMain ({ children }) {
+const TwoColumnMain = ({ children }) => {
   return <div className={styles.main}>{children}</div>
 }
 
-export function TwoColumnSidebar ({ children }) {
+const TwoColumnSidebar = ({ children }) => {
   return <div className={styles.sidebar}>{children}</div>
 }
+
+export { TwoColumn, TwoColumnMain, TwoColumnSidebar }
