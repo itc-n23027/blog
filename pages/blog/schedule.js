7c0671157c3800d249eeb598ddf5d0a137bc0fee
itc-n23027 <n23027@std.it-college.ac.jp>
2024-01-17 06:13:09 +0000

    schedule.js修正

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,61 +1,16 @@
 import { getPostBySlug } from 'lib/api'
-import { extractText } from 'lib/extract-text'
-import Meta from 'components/meta'
 import Container from 'components/container'
-import PostCategories from 'components/post-categories'
-import PostHeader from 'components/post-header'
-import PostBody from 'components/post-body'
-import {
-  TwoColumn,
-  TwoColumnMain,
-  TwoColumnSidebar
-} from 'components/two-column'
-import ConvertBody from 'components/convert-body'
-import Image from 'next/image'
 
 export default function Schedule ({
   title,
   publish,
   content,
   eyecatch,
-  categories,
-  description
+  categories
 }) {
   return (
     <Container>
-      <Meta
-        pageTitle={title}
-        pageDesc={description}
-        pageImg={eyecatch}
-        pageImgW={eyecatch.width}
-        pageImgH={eyecatch.height}
-      />
-      <article>
-        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
-
-        <figure>
-          <Image
-            src={eyecatch.url}
-            alt=''
-            layout='responsive'
-            width={eyecatch.width}
-            height={eyecatch.height}
-            sizes='(min-width: 1152px) 1152px, 100 vw'
-            priority
-          />
-        </figure>
-
-        <TwoColumn>
-          <TwoColumnMain>
-            <PostBody>
-              <ConvertBody contentHTML={content} />
-            </PostBody>
-          </TwoColumnMain>
-          <TwoColumnSidebar>
-            <PostCategories categories={categories} />
-          </TwoColumnSidebar>
-        </TwoColumn>
-      </article>
+      <h1>{title}</h1>
     </Container>
   )
 }
@@ -65,16 +20,13 @@ export async function getStaticProps () {
 
   const post = await getPostBySlug(slug)
 
-  const description = extractText(post.content)
-
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
       eyecatch: post.eyecatch,
-      categories: post.categories,
-      description: description
+      categories: post.categories
     }
   }
 }
