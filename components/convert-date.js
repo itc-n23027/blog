f52f0a180261ab5ee628368def792a3a3a60e17a
itc-n23027 <n23027@std.it-college.ac.jp>
2024-01-18 06:34:35 +0000

    修正

--- components/convert-date.js
+++ components/convert-date.js
@@ -4,9 +4,7 @@ import ja from 'date-fns/locale/ja'
 export default function ConvertDate ({ dateISO }) {
   return (
     <time dateTime={dateISO}>
-      {format(parseISO(dateISO), 'yyyy年MM月dd日', {
-        locale: ja
-      })}
+      {format(parseISO(dateISO), 'yyyy年MM月dd日', { locale: ja })}
     </time>
   )
 }
