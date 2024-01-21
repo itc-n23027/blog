649a5c225c331c25f712c9f55593381aac0e0879
itc-n23027 <n23027@std.it-college.ac.jp>
2024-01-21 08:01:19 +0000

    すべての記事のタイトルを追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,8 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
 export default function Blog ({ posts }) {
   return (
@@ -8,6 +10,18 @@ export default function Blog ({ posts }) {
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
 
       <Hero title='Blog' subtitle='Recent Posts' />
+
+      <Posts posts={posts} />
     </Container>
   )
+}
+
+export async function getStaticProps () {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
 }
